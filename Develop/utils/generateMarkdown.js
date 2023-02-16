39a55910d8d0bc39dcf076f796add0507c2a3278
Julianne <juliaguas17@gmail.com>
2023-02-15 20:44:49 -0600

    added semi-colon line 87

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -84,7 +84,7 @@ function generateMarkdown(data) {
 
   // Questions
   markdown += "# Questions\n";
-  markdown += "I can be reached via email or Github with further questions.\n"
+  markdown += "I can be reached via email or Github with further questions.\n";
   markdown += "Github: github.com/" + data.github + "\n";
   markdown += "Email: " + data.email + "\n";
 
