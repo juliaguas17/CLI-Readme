57cba7f139a15fadf5fa3a61bd529daa54e02fde
Julianne <juliaguas17@gmail.com>
2023-02-15 19:52:52 -0600

    fixed error in renderLicenseLink()

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,7 +12,7 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
 
 let licenseLink;
 
@@ -32,6 +32,7 @@ let licenseLink;
   }
   
   return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
